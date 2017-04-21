714924ea810b414ffa04e85b2e7012b5d00b0cbb
rai <rai99cos@gmail.com>
2017-04-21 20:15:02 +0900

    delete miss link option and add new link option

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-DOT_FILES=(.zshrc .config .tmux.conf)
+DOT_FILES=(.zshrc .tmux.conf)
 
 for file in ${DOT_FILES[@]}
 do
     ln -sfv $HOME/dotfiles/$file $HOME/$file
 done
+
+ln -sfvn $HOME/dotfiles/.config $HOME/.config
